f77cce8edb4027f6e38d21e57b0d0ec6d8a7cd59
Ionut Nahoi <ioan.nahoi02@e-uvt.ro>
2023-05-12 18:25:12 +0300

    Initial commit

---
+++ src/main/java/com/hacktm/hackbe/HackBeApplication.java
@@ -0,0 +1,13 @@
+package com.hacktm.hackbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hacktm/hackbe/HackBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacktm.hackbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
